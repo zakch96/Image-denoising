6b97ab70c00ccb379b91fcfb0e2a95dd171bd396
zakch96 <chlihzakaria@gmail.com>
2019-02-28 13:06:41 +0100

    ISTA.py

--- ISTA.py
+++ ISTA.py
@@ -7,10 +7,9 @@ from pylab import *
 
 def norm(x):
     nrm =0
-    for i in rang(len(x)):
+    for i in range(len(x)):
         nrm += pow(x[i], 2)
-    nrm = sqrt(nrm)
-    return nrm
+    return sqrt(nrm)
 
 def partial_derivative(func, var=0, point=[]):
     args = point[:]
@@ -19,75 +18,65 @@ def partial_derivative(func, var=0, point=[]):
         return func(*args)
     return misc.derivative(wraps, point[var], dx = 1e-6)
 
- 
-def gradient_descent(alpha, x, y, ep=0.0001, max_iter=10000):
-    converged = False
-   iter = 0
-    m = x.shape[0] 
 
-    # initial theta
-    t0 = np.random.random(x.shape[1])
-    t1 = np.random.random(x.shape[1])
+def sgn(a):
+    signe = 0
+    if  a > 0:
+        signe = 1
+    elif a < 0:
+        signe = -1
+    else:
+        return 0
+    return signe
 
-    # total error, J(theta)
-    J = sum([(t0 + t1*x[i] - y[i])**2 for i in range(m)])
+def shrinkage_operator(x, i, alpha):
+    for i in range(len(x)):
+        x[i] = abs((abs(x[i]) - alpha)) * sgn(x[i])
+    return x
 
-    # Iterate Loop
-    while not converged:
-        # for each training sample, compute the gradient (d/d_theta j(theta))
-        grad0 = 1.0/m * sum([(t0 + t1*x[i] - y[i]) for i in range(m)]) 
-        grad1 = 1.0/m * sum([(t0 + t1*x[i] - y[i])*x[i] for i in range(m)])
-
-        # update the theta_temp
-        temp0 = t0 - alpha * grad0
-        temp1 = t1 - alpha * grad1
-    
-        # update theta
-        t0 = temp0
-        t1 = temp1
-
-        # mean squared error
-        e = sum( [ (t0 + t1*x[i] - y[i])**2 for i in range(m)] ) 
-
-        if abs(J-e) <= ep:
-            print 'Converged, iterations: ', iter, '!!!'
-            converged = True
-    
-        J = e   # update error 
-        iter += 1  # update iter
-    
-        if iter == max_iter:
-            print 'Max interactions exceeded!'
-            converged = True
-
-    return t0,t1
-
-"
-########################################################################################################################################
-
-def laplacien(g, x, Lambda):
-    L = ones(len(x))
+def gradient(x, y, lbd):
+    grad = np.ones(len(x))
     for r in range(len(x)):
-        L[r] = 2*(x[r] - g(x[r])) - Lambda*((x[r]-x[r-1])/abs(x[r]-x[r-1]))
-    return L
+        grad[r] = 2*(x[r] - y[r]) + lbd*sgn(x[r])
+    return grad
 
-def Ista(beta_1, beta_2, x_0, Lambda, n, ST):
-    x_i = ones(len(x_0)) 
-    x_i_1 = ones(len(x_0))
-    z_i = ones(len(x_0))
-    for i in range(len(x_0)):
-        x_i[i] = x_0[i]
-    for i in range(n):
-        L = laplacien(g, x_i, Lambda)
-        x_i_1= ST(z_i, beta_2)
-        for j in range(len(x_0)):
-            z_i[j] = x_i[j] + beta_2*L[j]
+def Ista(beta, alpha, x_0, y, lbd, iter, eps):
+    n = len(x_0)
+    x_i = ones(n) 
+    z_i = ones(n)
+    x_i = np.copy(x_0)
+
+    def _g(x_0, y,lbd):
+        diff = 0
+        for i in range(1,len(x_0)):
+            diff += x[r] - [r-1]
+        return np.sum((x_0-y)*(x_0-y)) + lbd * diff
+
+    for i in range(iter):
+        z_i = np.copy(x_i + beta * gradient(x_i, y, lbd))
+        x_i = shrinkage_operator(z_i, i, alpha)
+        print("norme(x_",i,") =", norm(x_i))
+        if norm(x_i)**2 < eps:
+            print("le nombre d'itérations est : ",iter)
+            return True
+    return False
 
 
-def ST():
-    x = array([-3, -1, 0, 4, 6, 8])
-    y = array([0, 0, 0, 12, 18, 24])
-    plot(x, y)
-    
-    show()
-            
+
+
+x_0 = np.array([-3, -1, 0, 4, 6, 8])
+y = np.array([0, 0, 0, 12, 18, 24])
+beta = 0.01
+alpha = 0.01
+lbd = 0.01
+iter = 1000000
+eps = 0.001
+
+
+
+print(Ista(beta, alpha, x_0, y, lbd, iter, eps))
+
+print("x = ", x_0, "\ny= ", y)
+
+
+
